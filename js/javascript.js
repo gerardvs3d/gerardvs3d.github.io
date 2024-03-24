846daac5d3f8c45277f41dca5e06b08835150b47
gerardvs3d <gerard.vivo@uvic.cat>
2024-03-24 03:18:04 +0100

    ADAPTAT_A_MOBIL_SUUUU

--- js/javascript.js
+++ js/javascript.js
@@ -26,3 +26,21 @@ function showSlides() {
 }
 
 $('iframe').aspectRatioKeeper()
+
+
+
+function showMenu() {
+  var text = document.getElementById("menulist");
+  text.style.display = "block";
+
+  document.getElementById('open').style.display = 'none';
+  document.getElementById('close').style.display = 'block';
+}
+
+function hideMenu() {
+  var text = document.getElementById("menulist");
+  text.style.display = "none";
+
+  document.getElementById('open').style.display = 'block';
+  document.getElementById('close').style.display = 'none';
+}
